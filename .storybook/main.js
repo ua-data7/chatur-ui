b7eefd25b6c4f04cf2b7fb00cdbcd8db10ec09d3
Paul Sarando <psarando@cyverse.org>
2024-01-11 13:44:38 -0700

    Remove Storybook onboarding addon and assests

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,17 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+    ],
+    framework: {
+        name: "@storybook/nextjs",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
 };
 export default config;
