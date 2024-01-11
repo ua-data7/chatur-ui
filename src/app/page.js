7a79a8fb758609df218525c552c96be8e2eabaf2
Megh <meghavarshini@gmail.com>
2024-01-11 12:30:48 -0700

    edits to demo model selection box

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 "use client";
 
 import Button from '@mui/joy/Button';
+import ModelSelect from '../components/ModelSelect'
 
 export default function Home() {
   return (
     <main>
       <Button variant="soft">Hello world</Button>
+      <ModelSelect />
     </main>
   )
 }
---
+++ src/components/ModelSelect.js
@@ -0,0 +1,11 @@
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
+
+export default function ModelSelect() {
+  return (
+    <Select defaultValue="Model1">
+      <Option value="Model1">Model1</Option>
+      <Option value="Model2">Model2</Option>
+    </Select>
+  );
+}
