f03becaf090e164e93bdb81e8e5aa42f5eed4b5d
Megh <meghavarshini@gmail.com>
2024-01-11 11:33:13 -0700

    edits to fix issues with messed up background (see issues)

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 "use client";
 
+import Typography from '@mui/joy/Typography';
 import Button from '@mui/joy/Button';
 
 export default function Home() {
   return (
     <main>
-      <Button variant="soft">Hello world</Button>
+    <div>
+    <Button variant="soft">Hello world</Button>
+    </div>
     </main>
   )
 }
