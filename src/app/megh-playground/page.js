148640c55d914fd38677aec4de520f4934209381
Megh <meghavarshini@gmail.com>
2024-01-11 13:35:36 -0700

    separate webpage for testing components, plus bug fixes for filepaths

--- src/app/megh-playground/page.js
+++ src/app/megh-playground/page.js
@@ -2,11 +2,13 @@
 
 import Typography from '@mui/joy/Typography';
 import Button from '@mui/joy/Button';
+import ModelSelect from '../../components/ModelSelect'
 
 export default function Home() {
   return (
     <main>
       <Button variant="soft">Hello world</Button>
+      <ModelSelect />
     </main>
   )
 }
