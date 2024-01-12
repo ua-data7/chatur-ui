1a4d492206793500190ed120ac16502625f3bd9a
mgwall17 <mgwall17@gmail.com>
2024-01-12 13:38:27 -0700

    removed interface declaration and formcontrol props

--- src/components/editProfile/CountrySelector.js
+++ src/components/editProfile/CountrySelector.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Autocomplete from '@mui/joy/Autocomplete';
 import AutocompleteOption from '@mui/joy/AutocompleteOption';
 import AspectRatio from '@mui/joy/AspectRatio';
-import FormControl, { FormControlProps } from '@mui/joy/FormControl';
+import FormControl from '@mui/joy/FormControl';
 import FormLabel from '@mui/joy/FormLabel';
 import ListItemDecorator from '@mui/joy/ListItemDecorator';
 import Typography from '@mui/joy/Typography';
@@ -49,8 +49,6 @@ export default function ContrySelector(props) {
     </FormControl>
   );
 }
-
-interface CountryType
 
 // From https://bitbucket.org/atlassian/atlaskit-mk-2/raw/4ad0e56649c3e6c973e226b7efaeb28cb240ccb0/packages/core/select/src/data/countries.js
 const countries = [
