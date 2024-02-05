119a8e9b9e0c10935e4c243170459540b483b6db
Michelle Yung <myung@email.arizona.edu>
2024-02-05 15:38:04 -0700

    Send API requests to course-specific url endpoint

--- src/contexts/courses/courses.js
+++ src/contexts/courses/courses.js
@@ -1,4 +1,4 @@
 export const courses = [
-  { id: "CSC 335", name: "Object Oriented Programming", endpoint: "csc335" },
-  { id: "RNR 353", name: "Introduction to Wildland Fire", endpoint: "rnr353" },
+  { id: "CSC 335", name: "Object Oriented Programming", endpoint: "" },
+  { id: "RNR 353", name: "Introduction to Wildland Fire", endpoint: "" },
 ];
